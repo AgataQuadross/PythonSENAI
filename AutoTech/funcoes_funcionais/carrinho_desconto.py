834ec4456e84860e28282e8e6039689858d1f05c
Saint Gaia <agata.quadross2006@gmail.com>
2026-03-21 17:31:01 -0300

    Adicionando coisas aqui e ali
     pq meu vs code não esta conectando com o github eu to usando o codespace, codar sem consequencias imediatas é um problema pq agora provavelmente tem 10 erros onde eu não precisava que eles estivessem

--- AutoTech/funcoes_funcionais/carrinho_desconto.py
+++ AutoTech/funcoes_funcionais/carrinho_desconto.py
@@ -1,47 +1,48 @@
-# def Carrinho():
-#     # roda em while
-#     # acessaa lista de peças
-#     # calcula total por quantidade da peça
-#     # apresentar o total
-#     continua = True
-#     while continua == True:
-#         compra_peca = input("Qual peça você gostaria de comprar? (digite uma por vez) ")
-#         quant_compra = input("Quantas dessa peça você vai levar? ")
-#         total = 0
+def Carrinho():
+    # roda em while
+    # acessaa lista de peças
+    # calcula total por quantidade da peça
+    # apresentar o total
+    continua = True
+    while continua == True:
+        compra_peca = input("Qual peça você gostaria de comprar? (digite uma por vez) ")
+        quant_compra = input("Quantas dessa peça você vai levar? ")
+        total = 0
 
-#         pecas = ListaPecas()
-
-#         for peca in pecas == compra_peca:
-#             peca = pecas.get(compra_peca, "Essa peça não existe")
-
-#             total = peca * quant_compra
-
+        pecas = ListaPecas()
+        carrinho = dict.fromkeys(compra_peca, quant_peca)
         
-        
-#         opcao = input("quer comprar mais?")
-#         if opcao == "s":
-#             continue
-#         elif opcao =="n":
-#             print(f"{total}")
-#             continua = False
-#         else:
-#             print("opção invalida")
+        for peca in pecas == compra_peca:
+            peca = pecas.get(compra_peca, "Essa peça não existe")
+
+            total = peca * quant_compra
+
+        nota_fiscal = [{carrinho}, total]
+            
+        opcao = input("quer comprar mais?")
+        if opcao == "s":
+            continue
+        elif opcao =="n":
+            return nota_fiscal
+            continua = False
+        else:
+            print("opção invalida")
 
 
 
 
 
-# def Desconto():
-#     # acima de 500 == 10% de desconto
-#     # 18% de imposto icms
-#     total = Carrinho(total)
-#     desconto = total
+def Desconto():
+    # acima de 500 == 10% de desconto
+    # 18% de imposto icms
+    total = Carrinho(total)
+    desconto = total
 
-#     if total >= 500:
-#         desconto = total * 100 / 0.10
+    if total >= 500:
+        desconto = total * 100 / 0.10
     
-#     taxa = desconto * 100 / 0.18
+    taxa = desconto * 100 / 0.18
     
-#     total_desc_taxa = total + taxa
+    total_desc_taxa = total + taxa
 
-#     return total_desc_taxa
+    return total_desc_taxa
